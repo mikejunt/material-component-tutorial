2fc94ce00301caf6d4b37f666620b223ce101e4c
mikejunt <mikejunt@gmail.com>
2020-02-27 11:23:25 -0600

    all of them pls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TargetComponent } from './target/target.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TargetComponent
   ],
   imports: [
     BrowserModule,
